4c6514a2b5cbce5547ed334bde98b6cbd76c235e
Serge LEWDEN <slewden2@gmail.com>
2021-03-02 21:13:07 +0100

    Cleaning

--- FillDbLibrary/Bdd/DataBaseProvider.cs
+++ FillDbLibrary/Bdd/DataBaseProvider.cs
@@ -1,24 +1,47 @@
-﻿using Microsoft.Extensions.Configuration;
-using System;
+﻿using System;
 using System.Data;
 using System.Data.Common;
 
 namespace FillDbLibrary.Bdd
 {
+  /// <summary>
+  ///  Factory pour la construction et le maintien d'une connection
+  /// </summary>
+  /// <typeparam name="TProvider">LE type de fournisseur de base</typeparam>
   public static class DatabaseProvider<TProvider> where TProvider : DatabaseProvider
   {
+    /// <summary>
+    /// Fournit le cnnecteur 
+    /// </summary>
     public static TProvider Instance {  get; } = Activator.CreateInstance<TProvider>();
   }
 
 
+  /// <summary>
+  /// Classe de base pour les provider de connexion à une base de données
+  /// </summary>
   public abstract class DatabaseProvider
   {
+    /// <summary>
+    /// Obtient une valeur indiquant le fournisseur de database
+    /// </summary>
     public abstract DbProviderFactory Factory { get; }
 
+    /// <summary>
+    /// Ferme, nettoie et libère les ressources liées à la connexion
+    /// </summary>
     public virtual void Dispose() { }
 
+    /// <summary>
+    /// Renvoie la chaine de connexion à la base de données
+    /// </summary>
+    /// <returns>la chaine de connexion</returns>
     public abstract string GetConnectionString();
 
+    /// <summary>
+    /// Renvoie une connexion ouverte
+    /// </summary>
+    /// <returns>une connexion ouverte</returns>
     public DbConnection GetOpenConnection()
     {
       var conn = Factory.CreateConnection();
@@ -31,7 +54,11 @@ namespace FillDbLibrary.Bdd
 
       return conn;
     }
-    
+
+    /// <summary>
+    /// Renvoie une connexion fermée (mais prête à être ouverte)
+    /// </summary>
+    /// <returns>une connexion fermée</returns>
     public DbConnection GetClosedConnection()
     {
       var conn = Factory.CreateConnection();
@@ -51,9 +78,5 @@ namespace FillDbLibrary.Bdd
       p.Value = value ?? DBNull.Value;
       return p;
     }
-
-    protected static string GetConnectionString(IConfiguration configuration, string name)
-                            => configuration.GetConnectionString(name);
-
   }
 }
--- FillDbLibrary/Bdd/SqlServerDataBaseProvider.cs
+++ FillDbLibrary/Bdd/SqlServerDataBaseProvider.cs
@@ -5,20 +5,39 @@ using System.Data.Common;
 
 namespace FillDbLibrary.Bdd
 {
+  /// <summary>
+  /// Fournit un moyen unifié de se connecter à un serveur SQL Server
+  /// </summary>
   public sealed class SqlServerDataBaseProvider : DatabaseProvider
   {
     private const string APPSETTINGCONNECTIONSTRINGRUBRIQUE = "base";
     private readonly string configurationString;
 
+    /// <summary>
+    /// Initialise une nouvelle instance de la classe <see cref="SqlServerDataBaseProvider"/>
+    /// </summary>
     public SqlServerDataBaseProvider()
     {
       var config = AppSettingsJSonConfiguration.Configuration;
       this.configurationString = config.GetConnectionString(APPSETTINGCONNECTIONSTRINGRUBRIQUE);
     }
 
+    /// <summary>
+    /// Obtient une valeur indiquant le fournisseur de database
+    /// </summary>
     public override DbProviderFactory Factory => System.Data.SqlClient.SqlClientFactory.Instance;
 
+    /// <summary>
+    /// Renvoie la chaine de connexion à la base de données
+    /// </summary>
+    /// <returns>la chaine de connexion</returns>
     public override string GetConnectionString() => configurationString;
+
+    /// <summary>
+    /// Renvoie une chaine de connexion
+    /// </summary>
+    /// <param name="mars">Indique si la connexiont doit accepter les jeux de résultats actifs multiples (Multiple Active Result Set)</param>
+    /// <returns>une connexion ouverte</returns>
     public DbConnection GetOpenConnection(bool mars)
     {
       if (!mars)
